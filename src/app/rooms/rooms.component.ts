ef0cc0453d084e0908d18b2d00ccbfa30dc6c607
Harshit <harshit.3851@gmail.com>
2023-02-16 00:28:08 +0530

    Angular Installation and Binding Syntax

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+
+@Component({
+  selector: 'hinv-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent {
+  hotelName = 'BakaSenpai'
+}
