49923f6c387a5026ffe6da7a9bbac4595944ae70
Harshit <harshit.3851@gmail.com>
2023-02-16 21:59:57 +0530

    Build-In directives

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { AppComponent } from '../app.component';
-
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -9,4 +8,44 @@ import { AppComponent } from '../app.component';
 })
 export class RoomsComponent {
   hotelName = 'BakaSenpai'
+  isHidden = false;
+  room?:Room = {
+    totalRooms:100,
+    availableRooms:20,
+    bookedRooms:40
+  }
+
+  roomList : RoomList[] = [
+    {
+      roomNo:101,
+      roomType: 'Deluxe Room' ,
+      amenities: 'Air Conditioner Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 50,
+      photos :'https://images.deepai.org/art-image/a4bfcad34c97466880c15b31544cb854/pastel-pink-yellow-blue-stars-graffiti-thumb.jpg' ,
+      checkInTime: new Date( '11-Nov-2022'),
+      checkOutTime: new Date( '12-Nov-2023'),
+    },
+    {
+      roomNo:102,
+      roomType: 'Deluxe Room' ,
+      amenities: 'Air Conditioner Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 50,
+      photos :'https://images.deepai.org/art-image/a4bfcad34c97466880c15b31544cb854/pastel-pink-yellow-blue-stars-graffiti-thumb.jpg' ,
+      checkInTime: new Date( '10-Nov-2023'),
+      checkOutTime: new Date( '12-Nov-2023'),
+    },
+    {
+      roomNo:103,
+      roomType: 'Private Room' ,
+      amenities: 'Air Conditioner Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 50,
+      photos :'https://images.deepai.org/art-image/a4bfcad34c97466880c15b31544cb854/pastel-pink-yellow-blue-stars-graffiti-thumb.jpg' ,
+      checkInTime: new Date( '10-Nov-2023'),
+      checkOutTime: new Date( '12-Nov-2023'),
+    },
+
+  ]
+  toogle() {
+    this.isHidden = !this.isHidden;
+  }
 }
