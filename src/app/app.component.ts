212294c00288cdb37c175279e86f6151b685fc5b
Harshit <harshit.3851@gmail.com>
2023-02-21 23:28:52 +0530

    ViewChild ViewChildren and AfterViewInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef,AfterViewInit, ElementRef } from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'hinv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'hotelinventoryapp';
+
+  @ViewChild('user' , {read:ViewContainerRef}) vcr!:ViewContainerRef;
+  @ViewChild('name') name!:ElementRef;
+  ngAfterViewInit() {
+    const componentRef = this.vcr.createComponent(RoomsComponent)
+    console.log("cf",componentRef)
+    console.log("nam",this.name);
+    this.name.nativeElement.innerText = 'Inside native element'
+  }
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,79 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewChild,
+  ViewChildren,
+  QueryList,
+} from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
-export class RoomsComponent implements OnInit {
-
+export class RoomsComponent implements OnInit, AfterViewInit {
+  @ViewChild(HeaderComponent) headerComponent!: HeaderComponent;
+  @ViewChildren(HeaderComponent) headerComponents!: QueryList<HeaderComponent>;
   constructor() {}
-  hotelName = 'BakaSenpai'
+  hotelName = 'BakaSenpai';
   isHidden = false;
-  roomList:RoomList[] = []
-  roomSelected!:RoomList;
-  room?:Room = {
-    totalRooms:100,
-    availableRooms:20,
-    bookedRooms:40
-  }
-  ngOnInit(): void {   
-      this.roomList= [
+  roomList: RoomList[] = [];
+  roomSelected!: RoomList;
+  room?: Room = {
+    totalRooms: 100,
+    availableRooms: 20,
+    bookedRooms: 40,
+  };
+  ngOnInit(): void {
+    console.log(this.headerComponent);
+    this.roomList = [
       {
-        roomNo:101,
-        roomType: 'Deluxe Room' ,
+        roomNo: 101,
+        roomType: 'Deluxe Room',
         amenities: 'Air Conditioner Free Wi-Fi, TV, Bathroom, Kitchen',
         price: 1000,
-        photos :'https://images.deepai.org/art-image/a4bfcad34c97466880c15b31544cb854/pastel-pink-yellow-blue-stars-graffiti-thumb.jpg' ,
-        checkInTime: new Date( '11-Nov-2022'),
-        checkOutTime: new Date( '12-Nov-2023'),
-        rating:4.32343
+        photos:
+          'https://images.deepai.org/art-image/a4bfcad34c97466880c15b31544cb854/pastel-pink-yellow-blue-stars-graffiti-thumb.jpg',
+        checkInTime: new Date('11-Nov-2022'),
+        checkOutTime: new Date('12-Nov-2023'),
+        rating: 4.32343,
       },
       {
-        roomNo:102,
-        roomType: 'Deluxe Room' ,
+        roomNo: 102,
+        roomType: 'Deluxe Room',
         amenities: 'Air Conditioner Free Wi-Fi, TV, Bathroom, Kitchen',
         price: 1800,
-        photos :'https://images.deepai.org/art-image/a4bfcad34c97466880c15b31544cb854/pastel-pink-yellow-blue-stars-graffiti-thumb.jpg' ,
-        checkInTime: new Date( '10-Nov-2023'),
-        checkOutTime: new Date( '12-Nov-2023'),
-        rating:4.43112
+        photos:
+          'https://images.deepai.org/art-image/a4bfcad34c97466880c15b31544cb854/pastel-pink-yellow-blue-stars-graffiti-thumb.jpg',
+        checkInTime: new Date('10-Nov-2023'),
+        checkOutTime: new Date('12-Nov-2023'),
+        rating: 4.43112,
       },
       {
-        roomNo:103,
-        roomType: 'Private Room' ,
+        roomNo: 103,
+        roomType: 'Private Room',
         amenities: 'Air Conditioner Free Wi-Fi, TV, Bathroom, Kitchen',
         price: 5000,
-        photos :'https://images.deepai.org/art-image/a4bfcad34c97466880c15b31544cb854/pastel-pink-yellow-blue-stars-graffiti-thumb.jpg' ,
-        checkInTime: new Date( '10-Nov-2023'),
-        checkOutTime: new Date( '12-Nov-2023'),
-        rating:4.52322
+        photos:
+          'https://images.deepai.org/art-image/a4bfcad34c97466880c15b31544cb854/pastel-pink-yellow-blue-stars-graffiti-thumb.jpg',
+        checkInTime: new Date('10-Nov-2023'),
+        checkOutTime: new Date('12-Nov-2023'),
+        rating: 4.52322,
       },
-  
-    ]
+    ];
   }
+
+  ngAfterViewInit(): void {
+    console.log('hc', this.headerComponent);
+    console.log('hcs',this.headerComponents)
+    this.headerComponent.title = 'Baaka Senpai';
+  }
+
   toogle() {
     this.isHidden = !this.isHidden;
   }
 
-  selectedRoom(event:RoomList) {
-    this.roomSelected = event
+  selectedRoom(event: RoomList) {
+    this.roomSelected = event;
     console.log(event);
   }
 
   addRoom() {
-    let room:RoomList = ({
-      roomNo:104,
-      roomType: 'Guest Room' ,
+    let room: RoomList = {
+      roomNo: 104,
+      roomType: 'Guest Room',
       amenities: 'Air Conditioner Free Wi-Fi, TV, Bathroom, Kitchen',
       price: 500,
-      photos :'https://images.deepai.org/art-image/a4bfcad34c97466880c15b31544cb854/pastel-pink-yellow-blue-stars-graffiti-thumb.jpg' ,
-      checkInTime: new Date( '11-Nov-2022'),
-      checkOutTime: new Date( '12-Nov-2023'),
-      rating:4.0
-    });
+      photos:
+        'https://images.deepai.org/art-image/a4bfcad34c97466880c15b31544cb854/pastel-pink-yellow-blue-stars-graffiti-thumb.jpg',
+      checkInTime: new Date('11-Nov-2022'),
+      checkOutTime: new Date('12-Nov-2023'),
+      rating: 4.0,
+    };
     // this.roomList.push(room);
-    this.roomList = [...this.roomList,room]
+    this.roomList = [...this.roomList, room];
   }
 }
